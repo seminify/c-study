5e74898fb468a1b7a6c4cf0f0eaf11b30d484f16
seminify <75173035+seminify@users.noreply.github.com>
2023-05-05 00:00:06 +0900

    chapter22

---
+++ StructValAddress.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct point {
+    int xpos;
+    int ypos;
+};
+struct employee {
+    char name[20];
+    char phoneNum[20];
+    int age;
+};
+
+int main(void) {
+    struct point pos = {10, 20};
+    struct employee man = {"이승기", "010-1212-0001", 21};
+    printf("%p %p\n", &pos, &pos.xpos);
+    printf("%p %p\n", &man, man.name);
+    return 0;
+}
