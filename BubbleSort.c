430b5de1df4f8dc80da6b272a5882cb52674d5fb
seminify <75173035+seminify@users.noreply.github.com>
2023-05-05 00:00:06 +0900

    chapter15

---
+++ BubbleSort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void DesSort(int ary[], int len);
+
+int main(void) {
+    int arr[4] = {3, 2, 1, 4};
+    int i;
+    DesSort(arr, sizeof(arr) / sizeof(int));
+    for (i = 0; i < 4; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
+
+void DesSort(int ary[], int len) {
+    int i, j;
+    int temp;
+    for (i = 0; i < len - 1; i++)
+        for (j = 0; j < (len - i) - 1; j++)
+            if (ary[j] > ary[j + 1]) {
+                temp = ary[j];
+                ary[j] = ary[j + 1];
+                ary[j + 1] = temp;
+            }
+}
